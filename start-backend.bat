895c19421996d7b40b528cc95051bc7b13b44312
yogesh001-gif <yahlawat1980@gmail.com>
2026-01-25 01:21:49 +0530

    Delete unnecessary files and node_modules from project

--- start-backend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting AI Finance Advisor Backend Server with Gamification...
-cd /d "%~dp0\backend"
-echo.
-echo Backend server will start on http://localhost:5000
-echo Features: Transactions, AI Advice, Gamification System
-echo.
-node server.js
-pause
